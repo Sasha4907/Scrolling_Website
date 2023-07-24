ffd1ab8b25b5dd61aa28a6e285b667a3b18cd041
Sasha Ped <aleksaped@gmail.com>
2023-07-25 00:14:18 +0300

    new repo

---
+++ script.js
@@ -0,0 +1,15 @@
+let sun = document.getElementById('sun');
+let mountain = document.getElementById('mountain');
+let font = document.getElementById('font');
+let text = document.getElementById('text');
+let header = document.querySelector('header');
+
+window.addEventListener('scroll', function(){
+    let value = window.scrollY;
+    sun.style.top = value * 1.05 + 'px';
+    mountain.style.top = value * 0.5 + 'px';
+    font.style.top = value * 0 + 'px';
+    text.style.marginRight = value * 4 + 'px';
+    text.style.marginTop = value * 1 + 'px';
+    header.style.top = value * 0.5 + 'px';
+})
